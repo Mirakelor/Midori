d400cad3df354c52fdda03c32ec99a7bd5fa7a47
Mirakelor <63326519+Mirakelor@users.noreply.github.com>
2023-07-11 07:52:37 +0800

    👩‍🍳

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -8,7 +8,7 @@ export const post: PostConfig = {
     use: ['Webmention'],
     style: 'boxed', // 评论系统栏样式: none / bordered / lifted / boxed
     webmention: {
-      username: 'https://monocle.js.org',
+      username: 'monocle.js.org',
       sortBy: 'created', // 排序方式: created / updated
       sortDir: 'down', // 排序顺序: up / down
       form: true, // 启用评论: true / false
